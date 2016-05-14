a7d154381677bbedb0bfe86eb63b87c0f6d51049
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2016-05-14 15:41:36 +0300

    Allah Acbar

--- app/js/index.js
+++ app/js/index.js
@@ -12,11 +12,20 @@
 
                 // init map
                 var map = new google.maps.Map(document.getElementById('googleMap'), options);
-
+                var image = {
+    url: '../img/kitchen2.jpg',
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(50, 50),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
                 var marker1 = new google.maps.Marker({
                         position: new google.maps.LatLng(49.832402, 23.966342),
                         map: map,
-                        title: 'Офіс та склад'
+                        title: 'Офіс та склад '+'../img/kitchen2.jpg',
+                        icon:image
                     });
 
                 (function(marker) {
@@ -53,13 +62,20 @@
 
 $(document).ready(function() {
  $("#owl-demo").owlCarousel({
- 	   
+       
         autoplay: true,
         autoplayTimeout: 5500,
         items: 1,                
         loop: true       
  });
  $(".but").click(function(){
- 	$(".nav li").slideToggle();
+    $(".nav li").slideToggle();
  });
+});
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({slideWidth: 180,
+    minSlides: 5,
+    maxSlides: 6,
+    slideMargin: 10});
 });
